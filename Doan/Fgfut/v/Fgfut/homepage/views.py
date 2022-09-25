2052f4b37a9069c855db6ae3a70cbce5a793d4dd
meanmustard <caio.braga.felipe@gmail.com>
2022-09-25 13:29:03 -0300

    Versão 0.1 do projeto

--- Doan/Fgfut/v/Fgfut/homepage/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render 
-from django.http import HttpResponse
-from django.views.generic import TemplateView
-
-
-def index(request):
-    return render(request, "telainicial/index.html")
-    
